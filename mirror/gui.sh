8ca88928336c38661ca0d216d751e59e9f9d35a6
termuxmirror <162041900+termuxmirror@users.noreply.github.com>
2024-03-03 19:25:08 +0100

    update gui.sh

--- mirror/gui.sh
+++ mirror/gui.sh
@@ -5,3 +5,9 @@ apt-get install x11-repo
 apt-get install tigervnc
 
 apt-get install python-tkinter
+
+echo 'export DISPLAY=":1"' >> ~/.bash_profile
+
+clear
+
+echo "Setup GUI Finish, run source ~/.bash_profile"
