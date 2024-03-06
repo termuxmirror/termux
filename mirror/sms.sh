b0c6c8c783fd8b969b3b2c943b802e0acfc2320d
termuxmirror <162041900+termuxmirror@users.noreply.github.com>
2024-03-06 20:15:43 +0100

    update sms.sh

---
+++ mirror/sms.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-get install python3
+
+apt-get install git
+
+git clone https://github.com/mfr-fr/sms-sender.git && cd sms-sender && pip install -r requirements.txt && python sms-sender.py
