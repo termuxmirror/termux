b27cd440f859f8f3b96148d86d343e47023493f7
termuxmirror <162041900+termuxmirror@users.noreply.github.com>
2024-03-07 14:07:12 +0100

    update tar.sh

--- mirror/tar.sh
+++ mirror/tar.sh
@@ -2,6 +2,12 @@
 
 curl -L https://termuxmirror.netlify.app/mirror/tarinstaller.sh -o tarinstaller.sh
 
+chmod +x tarinstaller.sh
+
+mv tarinstaller.sh .shell/
+
+echo 'alias tarin="$HOME/.shell/tarinstaller.sh"' >> $HOME/.bashrc
+
 clear
 
-echo "tarinstaller.sh erfolgreich installiert"
+echo 'Run "source $HOME/.bashrc" and then run tarin for install an tar.xz archive from my website'
